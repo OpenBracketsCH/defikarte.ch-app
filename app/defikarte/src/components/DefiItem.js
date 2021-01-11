0d263343a6073e3fed0c08fbb4d5c1de1a2ce10b
elektrolytmangel <marc.muehlheim@hftm.ch>
2021-01-11 18:19:13 +0100

    add open maps app for navigation

--- app/defikarte/src/components/DefiItem.js
+++ app/defikarte/src/components/DefiItem.js
@@ -2,26 +2,54 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { MaterialIcons, Feather } from '@expo/vector-icons';
 import { withNavigation } from 'react-navigation';
+import openMap from 'react-native-open-maps';
 
 const DefiItem = ({ defibrillator, navigation }) => {
   const latlng = { latitude: defibrillator.lat, longitude: defibrillator.lon };
+  const openingHours = () => {
+    if (defibrillator.tags.opening_hours) {
+      return (
+        <View style={styles.noWrapStyle}>
+          <Feather style={styles.inlineIconStyle} name='clock' />
+          <Text style={styles.openingHoursTextStyle}>{defibrillator.tags.opening_hours}</Text>
+        </View>
+      );
+    }
+    return null;
+  };
 
-  return (
-    <TouchableOpacity onPress={() => navigation.navigate('Main', { latlng })}>
-      <View style={styles.containerStyle}>
-        <Text style={styles.titleStyle}>{defibrillator.tags['defibrillator:location'] ?? defibrillator.tags.description ?? defibrillator.tags.operator ?? 'n/A'}</Text>
-        <View style={styles.inlineStyle}>
-          <MaterialIcons style={styles.inlineIconStyle} name='my-location' />
-          <Text style={styles.inlineTextStyle}>{defibrillator.distance}m
-          / {defibrillator.lat.toFixed(4)}, {defibrillator.lon.toFixed(4)}
-          </Text>
+  const phone = () => {
+    if (defibrillator.tags['emergency:phone']) {
+      return (
+        <View style={styles.noWrapStyle}>
           <MaterialIcons style={styles.inlineIconStyle} name='phone' />
           <Text style={styles.inlineTextStyle}>{defibrillator.tags['emergency:phone']}</Text>
-          <Feather style={styles.inlineIconStyle} name='clock' />
-          <Text>{defibrillator.tags.opening_hours}</Text>
         </View>
+      );
+    }
+    return null;
+  };
+
+  const name = defibrillator.tags['defibrillator:location'] ?? defibrillator.tags.description ?? defibrillator.tags.operator ?? 'n/A';
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate('Main', { latlng })}>
+      <View style={styles.outsideContainerStyle}>
+        <View style={styles.containerStyle}>
+          <Text style={styles.titleStyle}>{name}</Text>
+          <View style={styles.inlineStyle}>
+            <MaterialIcons style={styles.inlineIconStyle} name='my-location' />
+            <Text style={styles.inlineTextStyle}>{defibrillator.distance}m
+          / {defibrillator.lat.toFixed(4)}, {defibrillator.lon.toFixed(4)}
+            </Text>
+            {phone()}
+            {openingHours()}
+          </View>
+        </View>
+        <TouchableOpacity onPress={() => openMap({ latitude: defibrillator.lat, longitude: defibrillator.lon, query: name, travelType: 'walk' })}>
+          <Feather style={styles.navigationIconStyle} name='navigation' />
+        </TouchableOpacity>
       </View>
-    </TouchableOpacity>
+    </TouchableOpacity >
   );
 }
 
@@ -38,14 +66,34 @@ const styles = StyleSheet.create({
   inlineStyle: {
     flexDirection: 'row',
     alignItems: 'center',
+    flexWrap: 'wrap',
   },
   inlineTextStyle: {
     fontSize: 16,
   },
+  noWrapStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  openingHoursTextStyle: {
+    flexWrap: 'wrap',
+    fontSize: 16,
+    flexShrink: 1,
+  },
   containerStyle: {
     padding: 5,
+    width: 300,
+  },
+  outsideContainerStyle: {
+    flexDirection: 'row',
     borderColor: 'lightgrey',
     borderWidth: 0.3,
+    alignItems: 'center'
+  },
+  navigationIconStyle: {
+    fontSize: 48,
+    marginRight: 20,
+    color: '#007AFF',
   }
 });
 
