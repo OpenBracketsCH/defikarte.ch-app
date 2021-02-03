64ae994ffc57c012e4801b229afc7631c0b6714f
MUEHLHEIM Marc <marc.muehlheim@hexagonsi.com>
2021-02-03 20:52:10 +0100

    Fix typo

--- app/defikarte/app.config.js
+++ app/defikarte/app.config.js
@@ -1,6 +1,6 @@
 export default ({ config }) => {
   const googleMapsApiKey = process.env['REACT_NATIVE_GOOGLE_MAPS_API_KEY']
-  config.andorid.config.googleMaps.apiKey = googleMapsApiKey;
+  config.android.config.googleMaps.apiKey = googleMapsApiKey;
   return {
     ...config,
   };
