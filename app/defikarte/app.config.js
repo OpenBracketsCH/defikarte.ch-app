204c6911ddb800711c56ee411ef030e6a8f373d5
marc <marc.muehlheim@hftm.ch>
2021-02-07 12:00:05 +0100

    Only override the google maps api key when it is provided

--- app/defikarte/app.config.js
+++ app/defikarte/app.config.js
@@ -1,6 +1,8 @@
 export default ({ config }) => {
   const googleMapsApiKey = process.env['REACT_NATIVE_GOOGLE_MAPS_API_KEY']
-  config.android.config.googleMaps.apiKey = googleMapsApiKey;
+  if (googleMapsApiKey !== null) {
+    config.android.config.googleMaps.apiKey = googleMapsApiKey;
+  }
   return {
     ...config,
   };
