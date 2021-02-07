b5800bb35c620c97e1f6c7471b3bb108282d29e4
marc <marc.muehlheim@hftm.ch>
2021-02-07 15:52:00 +0100

    Fix urls in AboutScreen #62

--- app/defikarte/src/screens/AboutScreen.js
+++ app/defikarte/src/screens/AboutScreen.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { ScrollView, Image, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Image, Text, Linking, StyleSheet } from 'react-native';
 
 const AboutScreen = ({ navigation }) => {
   return (
     <ScrollView style={styles.containerStyle}>
       <Image style={styles.imageStyle} source={require('../../assets/logo_defikarte.png')} />
-      <Text style={styles.titleStyle}>Das Projekt</Text>
-      <Text style={styles.textStyle}>Die Defikarte.ch-App hilft dabei, den nächsten Defibrillator in deiner Nähe zu finden. Über die Navigations-App des jeweiligen Gerätes, kannst du dich zu diesem navigieren lassen. So kann möglichst rasch einer Person in Not geholfen werden. Die Daten sind Open Source und werden von der Community in OpenStreetMaps (OSM) gepflegt und verwaltet. Da es in der Schweiz keinen kompletten Datensatz und auch keine Meldepflicht für Defibrillatoren gibt, sind nicht alle erfasst und somit auch nicht in der App ersichtlich. Die OSM-Community ist bemüht sich, die Daten aktuell und vollständig zu halten. Bemerkst du also, dass ein Defibrillator nicht eingetragen ist, unterstütze die Community und den guten Zweck indem du den fehlenden Defibrillator mithilfe dieser App mit Leichtigkeit erfasst.</Text>
-      <Text style={styles.titleStyle}>Webseite</Text>
-      <Text style={styles.textStyle}>https://www.defikarte.ch</Text>
-      <Text style={styles.titleStyle}>Lizenz</Text>
-      <Text style={styles.textStyle}>MIT-Lizenz (https://opensource.org/licenses/MIT)</Text>
-      <Text style={styles.titleStyle}>Mitmachen & Fehler melden auf Github</Text>
-      <Text style={styles.textStyle}>https://github.com/chnuessli/defikarte.ch-app</Text>
+      <View style={styles.wrapperStyle}>
+        <Text style={styles.titleStyle}>Das Projekt</Text>
+        <Text style={styles.textStyle}>Die Defikarte.ch-App hilft dabei, den nächsten Defibrillator in deiner Nähe zu finden. Über die Navigations-App des jeweiligen Gerätes, kannst du dich zu diesem navigieren lassen. So kann möglichst rasch einer Person in Not geholfen werden. Die Daten sind Open Source und werden von der Community in OpenStreetMaps (OSM) gepflegt und verwaltet. Da es in der Schweiz keinen kompletten Datensatz und auch keine Meldepflicht für Defibrillatoren gibt, sind nicht alle erfasst und somit auch nicht in der App ersichtlich. Die OSM-Community ist bemüht, die Daten aktuell und vollständig zu halten. Bemerkst du also, dass ein Defibrillator nicht eingetragen ist, unterstütze die Community und den guten Zweck indem du den fehlenden Defibrillator mithilfe dieser App mit Leichtigkeit erfasst.</Text>
+      </View>
+      <View style={styles.wrapperStyle}>
+        <Text style={styles.titleStyle}>Webseite</Text>
+        <Text
+          style={styles.linkStyle}
+          onPress={() => Linking.openURL('https://www.defikarte.ch')}>
+          https://www.defikarte.ch
+          </Text>
+      </View>
+      <View style={styles.wrapperStyle}>
+        <Text style={styles.titleStyle}>Lizenz</Text>
+        <Text style={styles.textStyle}>MIT (
+          <Text
+            style={styles.linkStyle}
+            onPress={() => Linking.openURL('https://opensource.org/licenses/MIT')}>
+            https://opensource.org/licenses/MIT
+              </Text>
+          )
+          </Text>
+      </View>
+      <View style={styles.wrapperStyle}>
+        <Text style={styles.titleStyle}>Mitmachen & Fehler melden auf Github</Text>
+        <Text
+          style={styles.linkStyle}
+          onPress={() => Linking.openURL('https://github.com/chnuessli/defikarte.ch-app')}
+        >
+          https://github.com/chnuessli/defikarte.ch-app
+          </Text>
+      </View>
     </ScrollView>
   );
 }
@@ -20,18 +44,25 @@ const AboutScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   containerStyle: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
   titleStyle: {
     fontSize: 20,
     alignSelf: 'center',
     fontWeight: '500',
-    marginTop: 10,
+  },
+  wrapperStyle: {
+    margin: 10,
   },
   textStyle: {
     fontSize: 16,
     alignSelf: 'center',
-    marginHorizontal: 10,
+  },
+  linkStyle: {
+    fontSize: 16,
+    alignSelf: 'center',
+    color: 'blue',
+    textDecorationLine: 'underline',
   },
   imageStyle: {
     height: 150,
