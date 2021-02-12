feef25474c55fd6e79e4d07bc50d61ecdfaa431f
MUEHLHEIM Marc <marc.muehlheim@hexagonsi.com>
2021-02-12 11:52:11 +0100

    Add api key for backend call #9

--- app/defikarte/src/api/defikarteBackend.js
+++ app/defikarte/src/api/defikarteBackend.js
@@ -2,4 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: process.env['REACT_NATIVE_BASE_URL'] ?? 'https://defikarte-backend-staging.azurewebsites.net/api',
+  'x-functions-clientid': 'defikarte-app',
+  'x-functions-key': process.env['REACT_NATIVE_API_KEY'] ?? '',
 })
