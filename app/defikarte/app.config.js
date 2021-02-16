fb912d6aa52d65c648399fa1cd54f0507f3d3034
MUEHLHEIM Marc <marc.muehlheim@hexagonsi.com>
2021-02-16 07:31:34 +0100

    Add usage of .env file

--- app/defikarte/app.config.js
+++ app/defikarte/app.config.js
@@ -1,5 +1,8 @@
+import 'dotenv/config';
+
 export default ({ config }) => {
   const googleMapsApiKey = process.env['REACT_NATIVE_GOOGLE_MAPS_API_KEY']
+  console.log(googleMapsApiKey)
   if (googleMapsApiKey != null) {
     config.android.config.googleMaps.apiKey = googleMapsApiKey;
   }
