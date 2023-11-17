498674591b16c5b5d1d6e562fe07464544a5b90b
Marc Muehlheim <marc.muehlheim@hftm.ch>
2023-11-17 17:07:52 +0100

    Fix issue with imediately crashes on startup

---
+++ app/defikarte/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: false,
+  extends: "@react-native-community",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    // Your custom rules go here
+  },
+};
--- app/defikarte/app.config.js
+++ app/defikarte/app.config.js
@@ -1,21 +1,6 @@
-import 'dotenv/config';
-
-export default ({ config }) => {
-  const googleMapsApiKey = process.env['REACT_NATIVE_GOOGLE_MAPS_API_KEY']
-  if (googleMapsApiKey != null) {
-    config.android.config.googleMaps.apiKey = googleMapsApiKey;
-  }
+module.exports = ({ config }) => {
+  config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || '';
   return {
-    extra: {
-      backendBaseUrl: process.env['REACT_NATIVE_BASE_URL'] ?? '',
-      backendApiKey: process.env['REACT_NATIVE_API_KEY'] ?? '',
-      eas: {
-        projectId: "1d176347-a834-4c1c-92e7-c0f4637108a0",
-      },
-    },
-    plugins: [
-      "expo-localization"
-    ],
     ...config,
   };
 };
